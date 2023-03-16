f75ba8645c2cf7cd4dcc4168a4ee99cdacd4df53
mpatskova <mpatskova@gmail.com>
2023-03-16 17:41:58 +0800

    Introducting tic-tac-toe to my portfolio.

--- Tic tac toe in Javascript/index.js
+++ Tic tac toe in Javascript/index.js
@@ -4,9 +4,27 @@ const startCells = [
     "","","","","","","","","",
 ]
 
+let go = "circle"
+infoDisplay.textContent = "Circle goes first"
+
 function createBoard() {
     startCells.forEach((cell, index) => {
         const cellElement = document.createElement("div")
-
+        cellElement.classList.add("square")
+        cellElement.id = index
+        cellElement.addEventListener("click", addGo)
+        gameBoard.append(cellElement)
     })
+}
+
+createBoard()
+
+
+function addGo(event) {
+    const goDisplay = document.createElement("div")
+    goDisplay.classList.add(go)
+    event.target.append(goDisplay)
+    go = go === "circle" ? "cross" : "circle" 
+    infoDisplay.textContent = "it is now " + go + "'s go."
+    event.target.removeEventListener("click", addGo)
 }
