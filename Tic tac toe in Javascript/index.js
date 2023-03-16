72a081dc5845f4b1e6089c135317741368145f51
mpatskova <mpatskova@gmail.com>
2023-03-16 16:48:33 +0800

    Created new folder for website and moved elements

---
+++ Tic tac toe in Javascript/index.js
@@ -0,0 +1,12 @@
+const gameBoard = document.querySelector("#gameboard")
+const infoDisplay = document.querySelector("#info")
+const startCells = [
+    "","","","","","","","","",
+]
+
+function createBoard() {
+    startCells.forEach((cell, index) => {
+        const cellElement = document.createElement("div")
+
+    })
+}
